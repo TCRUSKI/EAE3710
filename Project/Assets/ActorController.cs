ef1e66cf793d5c7194b6996146bf42330de86ae3
Eddie Overbay <32038687+TCRUSKI@users.noreply.github.com>
2023-04-19 16:15:58 -0600

    Add files via upload
    Added Animation Logic

--- Project/Assets/ActorController.cs
+++ Project/Assets/ActorController.cs
@@ -20,6 +20,24 @@ public class ActorController : MonoBehaviour
     protected GameObject characterArt;
     protected AudioSource audioSource;
 
+    public List<Material> climbMats;
+    protected int climbMatIndex;
+    protected bool climbing;
+    protected int climbDir;
+
+    public List<Material> runMats;
+    protected int runMatIndex;
+    protected bool running;
+
+    public List<Material> jumpMats;
+    protected int jumpMatIndex;
+    protected bool jumping;
+    protected bool falling;
+
+    public Material hangingMat;
+    protected bool hanging;
+    
+
     protected Queue<(Texture, int)> animationQueue;
     public Material redMat;
     public Material DefaultMovement;
@@ -35,6 +53,14 @@ public class ActorController : MonoBehaviour
         framesSinceClimb = -1;
         characterArt = gameObject.transform.GetChild(1).gameObject;
         audioSource = gameObject.transform.GetChild(3).gameObject.GetComponent<AudioSource>();
+        climbMatIndex = 0;
+        climbing = false;
+        climbDir = 0;
+        runMatIndex = 0;
+        running = false;
+        jumpMatIndex = 0;
+        jumping = false;
+        falling = false;
     }
 
     
@@ -42,7 +68,52 @@ public class ActorController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        Debug.Log("Jumping: " + jumping);
+        Debug.Log("Hanging: " + hanging);
+        Debug.Log("Running: " + running);
+        Debug.Log("Falling: " + falling);
+        Debug.Log("Climbing: " + climbing);
         bool shiftDown = Input.GetKey(KeyCode.LeftShift);
+
+
+        if(running){
+            characterArt.GetComponent<Renderer>().sharedMaterial = runMats[runMatIndex/2];
+            if(shiftDown){
+                runMatIndex += 2;
+            } else {
+                runMatIndex++;
+            }
+            if(runMatIndex >= runMats.Count * 2){
+                runMatIndex = 0;
+            }
+        }
+
+        if(climbing){
+            if(!ladder){
+                climbing = false;
+            } else {
+                characterArt.GetComponent<Renderer>().sharedMaterial = climbMats[climbMatIndex/4];
+                if(characterArt.transform.localScale.x != climbDir*Math.Abs(characterArt.transform.localScale.x)){
+                    characterArt.transform.localScale = new Vector3(climbDir*Math.Abs(characterArt.transform.localScale.x), characterArt.transform.localScale.y, characterArt.transform.localScale.z);
+                    characterArt.transform.localPosition = new Vector3(characterArt.transform.localPosition.x, characterArt.transform.localPosition.y, -(climbDir*Math.Abs(characterArt.transform.localPosition.z)));
+                }
+            }
+        }
+
+        if(jumping){
+            characterArt.GetComponent<Renderer>().sharedMaterial = jumpMats[jumpMatIndex/4];
+            jumpMatIndex++;
+            if(jumpMatIndex >= jumpMats.Count * 4){
+                jumping = false;
+                falling = true;
+                jumpMatIndex = 0;
+            }
+        }
+
+
+
+
+
     
         if(Math.Sign(Input.GetAxis("Horizontal")) != Math.Sign(rb.velocity.z) && Math.Sign(rb.velocity.z) != 0 && Input.GetAxis("Horizontal") != 0){
 
@@ -52,10 +123,16 @@ public class ActorController : MonoBehaviour
             rb.AddForce(new Vector3(0,0,Math.Sign(Input.GetAxis("Horizontal"))*50), ForceMode.Acceleration);
         } else {
             if(Math.Sign(Input.GetAxis("Horizontal")) > 0) {
-                if(characterArt.transform.localScale.x != Math.Abs(characterArt.transform.localScale.x)){
+                if(characterArt.transform.localScale.x != Math.Abs(characterArt.transform.localScale.x) && !climbing){
                     characterArt.transform.localScale = new Vector3(Math.Abs(characterArt.transform.localScale.x), characterArt.transform.localScale.y, characterArt.transform.localScale.z);
                     characterArt.transform.localPosition = new Vector3(characterArt.transform.localPosition.x, characterArt.transform.localPosition.y, -Math.Abs(characterArt.transform.localPosition.z));
                 }
+
+                if(!running && IsGrounded() && !climbing){
+                    running = true;
+                    characterArt.transform.localScale = new Vector3((characterArt.transform.localScale.x * 2), characterArt.transform.localScale.y, characterArt.transform.localScale.z);
+                }
+
                 if(!shiftDown){
                     if(IsGrounded() && (audioSource.clip != walkSound || !audioSource.isPlaying)){
                         audioSource.clip = walkSound;
@@ -83,10 +160,16 @@ public class ActorController : MonoBehaviour
                     rb.velocity = rb.velocity - new Vector3(0, 0, rb.velocity.z/50);
                 }
             } else if (Math.Sign(Input.GetAxis("Horizontal")) < 0) {
-                if(characterArt.transform.localScale.x != -Math.Abs(characterArt.transform.localScale.x)){
+                if(characterArt.transform.localScale.x != -Math.Abs(characterArt.transform.localScale.x) && !climbing){
                     characterArt.transform.localScale = new Vector3(-Math.Abs(characterArt.transform.localScale.x), characterArt.transform.localScale.y, characterArt.transform.localScale.z);
                     characterArt.transform.localPosition = new Vector3(characterArt.transform.localPosition.x, characterArt.transform.localPosition.y, Math.Abs(characterArt.transform.localPosition.z));
                 }
+
+                if(!running && IsGrounded() && !climbing){
+                    running = true;
+                    characterArt.transform.localScale = new Vector3((characterArt.transform.localScale.x * 2), characterArt.transform.localScale.y, characterArt.transform.localScale.z);
+                }
+
                 if(!shiftDown){
                     if(IsGrounded() && (audioSource.clip != walkSound || !audioSource.isPlaying)){
                         audioSource.clip = walkSound;
@@ -117,8 +200,15 @@ public class ActorController : MonoBehaviour
                 if(audioSource.isPlaying && audioSource.clip != jumpSound){
                     audioSource.Stop();
                 }
+
+                if(running){
+                    running = false;
+                    runMatIndex = 0;
+                    characterArt.GetComponent<Renderer>().sharedMaterial = DefaultMovement;
+                    characterArt.transform.localScale = new Vector3((characterArt.transform.localScale.x / 2), characterArt.transform.localScale.y, characterArt.transform.localScale.z);
+                }
+
                 rb.AddForce(new Vector3(0,0,Math.Sign(Input.GetAxis("Horizontal"))*50), ForceMode.Acceleration);
-                characterArt.GetComponent<Renderer>().sharedMaterial = DefaultMovement;
             }
         } 
         
@@ -129,13 +219,24 @@ public class ActorController : MonoBehaviour
         if (Input.GetKey(KeyCode.Space)){
             if(IsGrounded())
             {
+                if(!jumping){
+                    jumping = true;
+                    if(running){
+                        running = false;
+                        runMatIndex = 0;
+                    }else {
+                        characterArt.transform.localScale = new Vector3((characterArt.transform.localScale.x * 2), characterArt.transform.localScale.y, characterArt.transform.localScale.z);
+                    }
+                }
                 audioSource.clip = jumpSound;
                 audioSource.loop = false;
                 audioSource.Play();
-                rb.velocity = rb.velocity + new Vector3(0, 2f, 0);
-            } else if(framesSinceGrounced < 5){
+                rb.velocity = rb.velocity + new Vector3(0, 4f, 0);
+            } else if(framesSinceGrounced < 10){
                 rb.velocity = rb.velocity + new Vector3(0, .5f, 0);
             } else if(ledge){
+                jumping = true;
+                hanging = false;
                 ledge = false;
                 framesSinceGrounced = 0;
                 rb.constraints = defaultConstraints;
@@ -144,6 +245,17 @@ public class ActorController : MonoBehaviour
         }
 
         if(IsGrounded()){
+            if(jumping){
+                jumping = false;
+                jumpMatIndex = 0;
+                characterArt.GetComponent<Renderer>().sharedMaterial = DefaultMovement;
+                characterArt.transform.localScale = new Vector3((characterArt.transform.localScale.x / 2), characterArt.transform.localScale.y, characterArt.transform.localScale.z);
+            }
+            if(falling){
+                falling = false;
+                characterArt.GetComponent<Renderer>().sharedMaterial = DefaultMovement;
+                characterArt.transform.localScale = new Vector3((characterArt.transform.localScale.x / 2), characterArt.transform.localScale.y, characterArt.transform.localScale.z);
+            }
             if(framesSinceGrounced != 0){
                 audioSource.clip = jumpSound;
                 audioSource.loop = false;
@@ -151,6 +263,16 @@ public class ActorController : MonoBehaviour
             }
             framesSinceGrounced = 0;
         } else {
+            if(!falling && !jumping && !climbing && !hanging){
+                falling = true;
+                characterArt.GetComponent<Renderer>().sharedMaterial = jumpMats[2];
+                if(running){
+                    running = false;
+                    runMatIndex = 0;
+                } else {
+                    characterArt.transform.localScale = new Vector3((characterArt.transform.localScale.x * 2), characterArt.transform.localScale.y, characterArt.transform.localScale.z);
+                }
+            }
             if(audioSource.isPlaying && audioSource.clip != jumpSound){
                 audioSource.Stop();
             }
@@ -171,16 +293,60 @@ public class ActorController : MonoBehaviour
                 ledge = true;
                 rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
                 framesSinceClimb = 0;
+                if(!hanging){
+                    hanging = true;
+                    characterArt.GetComponent<Renderer>().sharedMaterial = hangingMat;
+                    if(running){
+                        running = false;
+                        runMatIndex = 0;
+                    }
+                    if(jumping){
+                        jumping = false;
+                        jumpMatIndex = 0;
+                    }
+                    if(falling){
+                        falling = false;
+                    }
+                }
             }
         }
 
         if(ladder){
+            if(!climbing){
+                climbing = true;
+                if(running){
+                    running = false;
+                    runMatIndex = 0;
+                    characterArt.transform.localScale = new Vector3((characterArt.transform.localScale.x / 2), characterArt.transform.localScale.y, characterArt.transform.localScale.z);
+                }
+                if(jumping){
+                    jumping = false;
+                    jumpMatIndex = 0;
+                    characterArt.transform.localScale = new Vector3((characterArt.transform.localScale.x / 2), characterArt.transform.localScale.y, characterArt.transform.localScale.z);
+                }
+                if(falling){
+                    falling = false;
+                    characterArt.transform.localScale = new Vector3((characterArt.transform.localScale.x / 2), characterArt.transform.localScale.y, characterArt.transform.localScale.z);
+                }
+            }
             if(Math.Sign(Input.GetAxis("Vertical")) > 0) {
                 transform.position = transform.position + new Vector3(0, .1f, 0);
+                climbMatIndex++;
+                if(climbMatIndex >= climbMats.Count*4){
+                    climbMatIndex = 0;
+                }
             } else if (Math.Sign(Input.GetAxis("Vertical")) < 0){
-                transform.position = transform.position + new Vector3(0, -.1f, 0);
+                if(!IsGrounded()){
+                    transform.position = transform.position + new Vector3(0, -.1f, 0);
+                    climbMatIndex--;
+                    if(climbMatIndex <= 0){
+                        climbMatIndex = climbMats.Count*4 - 1;
+                    }
+                }
             }
         }
+
+        
 
         
     }
@@ -200,6 +366,7 @@ public class ActorController : MonoBehaviour
         if(collision.GetComponent<Collider>().gameObject.tag == "Ladder"){
             ladder = true;
             rb.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotation;
+            climbDir = collision.GetComponent<Collider>().gameObject.GetComponent<LadderScript>().climbDir;
         }
     }
 
@@ -214,9 +381,10 @@ public class ActorController : MonoBehaviour
     private bool IsGrounded() 
     { 
         RaycastHit hit;
-        bool flagL = Physics.Raycast(bc2d.bounds.center - new Vector3(0, bc2d.bounds.extents.z, 0), Vector3.down, out hit, bc2d.bounds.extents.y + 0.05f);
-        bool flagR = Physics.Raycast(bc2d.bounds.center + new Vector3(0, bc2d.bounds.extents.z, 0), Vector3.down, out hit, bc2d.bounds.extents.y + 0.05f);
-        return flagL || flagR;
+        bool flagL = Physics.Raycast(bc2d.bounds.center - new Vector3(0, bc2d.bounds.extents.z, 0), Vector3.down, out hit, bc2d.bounds.extents.y - 0.5f);
+        bool flagC = Physics.Raycast(bc2d.bounds.center, Vector3.down, out hit, bc2d.bounds.extents.y - 0.5f);
+        bool flagR = Physics.Raycast(bc2d.bounds.center + new Vector3(0, bc2d.bounds.extents.z, 0), Vector3.down, out hit, bc2d.bounds.extents.y - 0.5f);
+        return flagL || flagR || flagC;
     }
 
 
---
+++ Project/Assets/LadderScript.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderScript : MonoBehaviour
+{
+    public int climbDir;
+}
